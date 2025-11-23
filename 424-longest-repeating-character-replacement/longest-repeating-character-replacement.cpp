649b83c77a4f6981fed6c264be1292f763f101d7
RajeshNegi17 <rajeshnegi617@gmail.com>
2025-11-23 19:09:16 +0530

    Time: 12 ms (22.10%) | Memory: 11.1 MB (7.78%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int>freq;
+        unordered_map<char,int> freq;
+        int max_len = 0;
         int left=0;
-        int max_len = 0,window=0;
         for(int right=0;right<s.size();right++){
             freq[s[right]]++;
-            auto it = max_element( freq.begin(),freq.end() , [](const auto&a,const auto &b){
+            auto it = max_element(freq.begin(),freq.end(),[](const auto &a,const auto &b){
                 return a.second<b.second;
             });
-            int max_value = it->second;
-
-            while((right-left+1) - max_value > k){ //uisng trick cause no need to caluclate max_value for each loop
+            int max_count = it->second;
+            while(right - left+1 - max_count > k){
                 freq[s[left]]--;
                 left++;
             }
