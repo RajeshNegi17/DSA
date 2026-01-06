c54032971d7d451963f5712f2f04756dec4ee42c
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-01-06 14:30:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        //vector<int> memArray(n+1);
-
-        if ( n==2 || n==1){
+        vector<int>arr(n+1,-1);
+        arr[0] = 0;
+        arr[1] = 1;
+        if( n<=1 ){
             return n;
         }
-
-        // memArray[0] = 0;
-        // memArray[1] = 1;
-        // memArray[2] = 2;
-        // for(int i=3 ; i<=n; i++) {
-        //     memArray[i] = memArray[i-1] + memArray[i-2];
-        // }
-        // return memArray[n];
-        //further optimization we need only two values i-1 and i-2
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        for(int i=3; i<=n ;i++){
-            c = a+b;
-            a=b;
-            b=c;
+        arr[2] = 2;
+        for(int i=3;i<=n;i++){
+            arr[i] = arr[i-1] + arr[i-2];
         }
-        return c;
+        return arr[n];
     }
 };
+/*
+state transition
+arr[i] = 0,1,2,3,
+t[i] = arr[i-1] + arr[i-2];
+arr[4] = arr[3]+arr[2];
+*/
