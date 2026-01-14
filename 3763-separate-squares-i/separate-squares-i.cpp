885ba57c5a690f087edc166dac3e11f95052cf20
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-01-14 22:13:53 +0530

    Time: 105 ms (57.27%) | Memory: 198.7 MB (73.55%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    bool check(vector<vector<int>>& squares,double mid_y,double total){
+        double bottom_area = 0.00;
+         for(auto& square : squares) {
+            double y = square[1];
+            double l = square[2];
+            
+            double bottom_y = y;
+            double top_y = y+l;
+
+            if( mid_y >= top_y ){
+                bottom_area += l*l;
+            }
+            else if( mid_y < top_y && mid_y > bottom_y ) {
+                bottom_area += l*(mid_y - bottom_y);
+            }
+        }
+        return (bottom_area >= total/2.0);
+    }
+
+    double separateSquares(vector<vector<int>>& squares) {
+        double low = INT_MAX;
+        double high = INT_MIN;
+        double total = 0.00;
+
+        for(auto& square : squares) {
+            double x = square[0];
+            double y = square[1];
+            double l = square[2];
+            total += l*l;
+            low = min(low,y);
+            high = max(high,y+l);
+        }
+
+        double result_y = 0.00;
+
+        while( high-low > 1e-5) {
+            double mid_y = low + (high-low)/2;
+            result_y = mid_y;
+
+            if( check(squares,mid_y,total)==true ){
+                high = mid_y;
+            }
+            else{
+                low = mid_y;
+            }
+        }
+        return result_y;
+    }
+};
