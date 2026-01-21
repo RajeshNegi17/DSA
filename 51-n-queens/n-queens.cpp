660529884f38f0c2dfd06d03068e3443617cff48
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-01-21 21:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (40.53%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -3,22 +3,22 @@ public:
     vector<vector<string>> result;
     int N;
 
-    bool isvalid(vector<string>board,int row,int col){
-        //look upward
-        for(int i=row-1 ; i>=0 ;i--){
+    bool isvalid(vector<string>&board,int row, int col){
+        //upward check //koi queen upar to ni ha
+        for( int i=row-1; i>=0; i--){
             if(board[i][col]=='Q'){
                 return false;
             }
         }
-        //check diagonal left
-        for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j--){
+        //diagonal check left side
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--){
             if(board[i][j]=='Q'){
                 return false;
             }
         }
-        //check digaonal right
-        for(int i=row-1,j=col+1; i>=0 && j<N; i--,j++){
-            if(board[i][j]=='Q'){
+        //diagonal check right side
+        for(int i =row-1, j=col+1; i>=0 && j<N; i--,j++){
+            if(board[i][j] == 'Q'){
                 return false;
             }
         }
@@ -29,8 +29,8 @@ public:
             result.push_back(board);
             return;
         }
-        for(int col=0; col<N; col++){
-            if(isvalid(board,row,col)){
+        for(int col=0;col<N;col++){
+            if( isvalid(board,row,col)){
                 board[row][col] = 'Q';
                 solve(board,row+1);
                 board[row][col] = '.';
@@ -39,8 +39,8 @@ public:
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
         N=n;
+        vector<string>board(n,string(n,'.'));
         solve(board,0);
         return result;
     }
