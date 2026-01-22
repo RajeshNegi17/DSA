c5d20e81018947c5516c746b9245913631ec8e0b
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-01-22 17:22:39 +0530

    Time: 3 ms (71.38%) | Memory: 13.1 MB (78.79%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
     
-    bool isvalid(string &s){
-        // stack<char> st;
-        // for(int i=0; i<s.size(); i++){
-        //     if( s[i]=='('){
-        //         st.push(s[i]);
-        //     }
-        //     if(s[i]==')'){
-        //         if(st.empty()) return false;
-        //         else{
-        //             if(st.top()=='('){
-        //                 st.pop();
-        //             }
-        //         }
-        //     }
-        // }
-        // return (st.empty());
-        int count=0;
-        for(char&c : s){
-            if(c=='('){
-                count++;
-            }
-            else{
-                count--;
-            }
-            if( count<0 ){
-                return false;
-            }
-        }
-        return count==0;
-    }
+    // bool isvalid(string &s){
+    //     // stack<char> st;
+    //     // for(int i=0; i<s.size(); i++){
+    //     //     if( s[i]=='('){
+    //     //         st.push(s[i]);
+    //     //     }
+    //     //     if(s[i]==')'){
+    //     //         if(st.empty()) return false;
+    //     //         else{
+    //     //             if(st.top()=='('){
+    //     //                 st.pop();
+    //     //             }
+    //     //         }
+    //     //     }
+    //     // }
+    //     // return (st.empty());
+    //     int count=0;
+    //     for(char&c : s){
+    //         if(c=='('){
+    //             count++;
+    //         }
+    //         else{
+    //             count--;
+    //         }
+    //         if( count<0 ){
+    //             return false;
+    //         }
+    //     }
+    //     return count==0;
+    // }
 
-    void solve(string &s, int total,vector<string> &result){
+    void solve(string &s, int n,vector<string> &result, int open, int close){
         
-        if(s.length()==total){
-            if(isvalid(s)){
-                result.push_back(s);
-            }
-           return;
+        if(s.length()==2*n){
+            result.push_back(s);
+            return; 
         }
-        
-        s.push_back('(');
-        solve(s,total,result);
-        s.pop_back();
-
-        s.push_back(')');
-        solve(s,total,result);
-        s.pop_back();
+        if(open<n){
+            s.push_back('(');
+            solve(s,n,result,open+1,close);
+            s.pop_back();
+        }
+        if( close<open ){
+            s.push_back(')');
+            solve(s,n,result,open,close+1);
+            s.pop_back();
+        }
     }
 
     vector<string> generateParenthesis(int n) {
-        int total = 2*n;
+        // int total = 2*n;
         string s="";
         vector<string> result;
-        solve(s,total,result);
+        solve(s,n,result, 0,0);
         return result;
     }
 };
