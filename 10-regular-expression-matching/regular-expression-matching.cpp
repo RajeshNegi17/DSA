9411723cb5d99e49c6d0fde2dfe9548dd78fde41
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-01-26 17:47:18 +0530

    Time: 523 ms (6.04%) | Memory: 8 MB (95.98%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool solve(string s , string p){
-        if( p.length()==0 ){
-            if( s.length()==0 ){
+    bool solve(int i,int j,string &s , string &p){
+        if( j==p.length() ){
+            if( i==s.length() ){
                 return true;
             }
             return false;
         }
         bool first_char_check = false;
-        if( s.length()>0 && (p[0]==s[0] || p[0]=='.') ){
+        if( i<s.length() && (p[j]==s[i] || p[j]=='.') ){
             first_char_check = true;
         }
-        if( p[1]=='*' ){
-            bool not_take_star = solve(s,p.substr(2));
-            bool take_star = first_char_check && solve(s.substr(1),p);
+        if( j+1<p.length() && p[j+1]=='*' ){
+            bool not_take_star = solve(i,j+2,s,p);
+            bool take_star = first_char_check && solve(i+1,j,s,p);
 
             return take_star || not_take_star;
         }
-        return first_char_check && solve(s.substr(1),p.substr(1));
+        return first_char_check && solve(i+1,j+1,s,p);
     }
     bool isMatch(string s, string p) {
-        return solve(s,p);
+        return solve(0,0,s,p);
     }
 };
