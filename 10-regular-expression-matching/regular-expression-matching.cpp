da1049086a06f235812fbedd8360aac02bfa8de4
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-01-26 17:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (74.55%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int t[21][21];
     bool solve(int i,int j,string &s , string &p){
         if( j==p.length() ){
             if( i==s.length() ){
@@ -7,6 +8,11 @@ public:
             }
             return false;
         }
+
+        if( t[i][j] != -1 ){
+            return t[i][j];
+        }
+
         bool first_char_check = false;
         if( i<s.length() && (p[j]==s[i] || p[j]=='.') ){
             first_char_check = true;
@@ -15,11 +21,12 @@ public:
             bool not_take_star = solve(i,j+2,s,p);
             bool take_star = first_char_check && solve(i+1,j,s,p);
 
-            return take_star || not_take_star;
+            return t[i][j] = take_star || not_take_star;
         }
-        return first_char_check && solve(i+1,j+1,s,p);
+        return t[i][j] = first_char_check && solve(i+1,j+1,s,p);
     }
     bool isMatch(string s, string p) {
+        memset(t,-1,sizeof(t));
         return solve(0,0,s,p);
     }
 };
