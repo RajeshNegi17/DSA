87cc8f39392c3c84dd70069e885ff19a9d7dfd19
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-02-10 19:25:47 +0530

    Time: 1 ms (38.31%) | Memory: 17.1 MB (31.41%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    //[2 1 1 2]
-    int bfs(vector<vector<int>>& grid,queue<pair<int,int>>&q) {
-        int minute=-1;
-        int col = grid[0].size();
+    int bfs(vector<vector<int>>&grid , queue<pair<int,int>>& q){
+        int minute = -1;
         int row = grid.size();
-        vector<pair<int,int>> elem={{-1,0},{1,0},{0,-1},{0,1}};
+        int col = grid[0].size();
+        vector<vector<int>>elem={{-1,0},{0,1},{0,-1},{1,0}};
         while(!q.empty()){
-            int size = q.size(); //2
+            int size = q.size();
             minute++;
-            for(int loop=0;loop<size;loop++){
-                auto ele = q.front(); 
+            for(int loop = 0; loop<size ; loop++){
+                auto ele = q.front();
                 int i = ele.first;
                 int j = ele.second;
                 q.pop();
-                for (int t = 0; t<4; t++) {
-                    int new_i = i+elem[t].first;
-                    int new_j = j+elem[t].second;
-                    if ( new_i <0 || new_i>=row || new_j <0 || new_j>=col) {
+                for(int d = 0; d<4 ;d++){
+                    int new_i = i+elem[d][0];
+                    int new_j = j+elem[d][1];
+                    if( new_i<0 || new_i>=row || new_j<0 || new_j>=col){
                         continue;
                     }
-                    if( grid[new_i][new_j] == 1){
+                    if( grid[new_i][new_j]==1) {
                         q.push({new_i,new_j});
                         grid[new_i][new_j] = 2;
                     }
-
                 }
-            }
+            }  
         }
         return minute;
     }
+
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>> q;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        int row = grid.size();
+        int col = grid[0].size();
+        for( int i=0; i<row ;i++){
+            for(int j=0;j<col;j++){
                 if( grid[i][j]==2 ){
                     q.push({i,j});
                 }
@@ -41,13 +42,13 @@ public:
         }
         int result=0;
         result = bfs(grid,q);
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        for( int i=0; i<row ;i++){
+            for(int j=0;j<col;j++){
                 if( grid[i][j]==1 ){
-                   return -1;
+                    return -1;
                 }
             }
         }
-        return result<0 ? 0:result;
+        return result <0 ? 0 : result;
     }
 };
