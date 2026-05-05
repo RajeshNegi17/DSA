11e1c3de855b68b1eb65bebb5aa55f09567dd8c6
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-05-05 10:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (99.12%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int fast = 0,slow=0;
-        for( fast=0; fast<nums.size(); fast++ ){
-            if( nums[fast] != val ){
-                nums[slow] = nums[fast];
+        int fast = 0;
+        int slow = 0;
+        for(fast;fast<nums.size();fast++){
+            if( nums[fast] != val){
+                swap(nums[fast],nums[slow]);
                 slow++;
             }
         }
         return slow;
     }
 };
+
