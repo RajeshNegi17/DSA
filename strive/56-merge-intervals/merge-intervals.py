b7a4951838532dbdf3959f7773eb5e07d95a4ba2
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-08-05 12:33:40 +0530

    Time: 7 ms (71.62%) | Memory: 23.2 MB (15.91%) - LeetSync

--- strive/56-merge-intervals/merge-intervals.py
+++ strive/56-merge-intervals/merge-intervals.py
@@ -3,13 +3,10 @@ class Solution:
         intervals.sort()
         ans = [intervals[0]]
 
-        for start,end in intervals[1:]:
+        for start,end in intervals[1:len(intervals)]:
 
             if start <= ans[-1][1]:
                 ans[-1][1] = max(ans[-1][1],end)
             else:
                 ans.append([start,end])
-
         return ans
-
-        
