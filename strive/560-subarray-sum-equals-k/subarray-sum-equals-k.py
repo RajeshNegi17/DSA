69dd848be8bf15652e02091adbae07a4c6b71278
RajeshNegi17 <rajeshnegi617@gmail.com>
2026-08-31 12:10:39 +0530

    Time: 25 ms (94.37%) | Memory: 21.8 MB (84.90%) - LeetSync

--- strive/560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ strive/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix = 0
-        freq = {0:1}
-        count = 0
+        mp = {0:1}
+        sum = 0
+        ans = 0
         for num in nums:
-            prefix += num
-            need = prefix - k
-            if need in freq:
-                count += freq[need]
-            if prefix in freq:
-                freq[prefix] += 1
-            else:
-                freq[prefix] = 1
-        return count
+            sum += num
+            need  = sum - k
+            if need in mp:
+                ans += mp[need]
+            mp[sum] = mp.get(sum,0)+1
+        return ans
